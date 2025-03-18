70960db49e7ea7a330a1ac5c36dbe6e33de9dac4
Mahmoud-Heikal <mahmoud.samy@shahbandr.team>
2025-03-19 00:44:26 +0200

    custome response

---
+++ app/Traits/customResponseTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Traits;
+
+trait customResponseTrait
+{
+    function customResponse($result = null, $status = 200, $additional = []): \Illuminate\Http\JsonResponse
+    {
+
+        if ($status == 200) {
+            $success = true;
+            $Key = "result";
+        } else {
+            $success = false;
+            $Key = "error";
+        }
+
+        if (is_string($result)) {
+            $result = [
+                "message" => $result
+            ];
+        }
+
+        $data = [
+            "status" => $status,
+            "success" => $success,
+            $Key => $result,
+        ];
+
+        return response()->json(array_merge($data, $additional), $status);
+    }
+}
